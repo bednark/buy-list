fb2174b263ffd5d853fb4725528e6e1ace3693e5
Jakub Bednarek <kubabednarekk@gmail.com>
2025-05-11 13:31:48 +0200

    Added search input for recepies subpage

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -5,3 +5,20 @@ document.getElementById("copyYear").innerHTML = year;
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
 }
+
+const searchRecepie = document.getElementById("searchRecepie");
+
+searchRecepie.addEventListener("keyup", function (event) {
+  const searchValue = event.target.value.toLowerCase();
+  const noRecepies = document.getElementById("no-recepies");
+  const noKeyword = document.getElementById("no-keyword");
+
+  if(searchValue.length == 0) {
+    noKeyword.style.display = "block";
+    noRecepies.style.display = "none";
+  }
+  else {
+    noKeyword.style.display = "none";
+    noRecepies.style.display = "block";
+  }
+});
